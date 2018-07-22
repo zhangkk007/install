ed6d16a3ee969152fca2d583370187cf1dc28cbf
Troels Henriksen <athas@sigkill.dk>
2018-07-22 11:12:33 +0200

    Set umask to 022 before running installation (#34)
    This ensures that the /home/linuxbrew directory will be readable by
    the user.

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -eu
+umask 022
 which ruby >/dev/null || eval "`curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install-ruby`"
 exec ruby -e "`curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install`"
